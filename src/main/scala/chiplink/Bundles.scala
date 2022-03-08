d57d7f320d68f806637e85dd391053716a1f44f9
Bohan Hu <hubohancser@gmail.com>
2022-03-08 22:03:50 +0800

    [WIP] Add code comments

--- src/main/scala/chiplink/Bundles.scala
+++ src/main/scala/chiplink/Bundles.scala
@@ -6,6 +6,7 @@ import freechips.rocketchip.util.CompileOptions.NotStrictInferReset
 import freechips.rocketchip.util.{rightOR,GenericParameterizedBundle}
 
 class WideDataLayerPortLane(params: ChipLinkParams) extends GenericParameterizedBundle(params) {
+  // 单侧的通道
   val clk  = Clock(OUTPUT)
   val rst  = Bool(OUTPUT)
   val send = Bool(OUTPUT)
@@ -13,17 +14,26 @@ class WideDataLayerPortLane(params: ChipLinkParams) extends GenericParameterized
 }
 
 class WideDataLayerPort(params: ChipLinkParams) extends GenericParameterizedBundle(params) {
+  // 这个是定义在顶层的数据接口
+  // FPGA -> CPU和CPU -> FPGA
+  // 每一个接口都是上面那个WideDataLayerPortLane的形式
   val c2b = new WideDataLayerPortLane(params)
   val b2c = new WideDataLayerPortLane(params).flip
 }
 
 class DataLayer(params: ChipLinkParams) extends GenericParameterizedBundle(params) {
+  // 数据层，32位的数据
   val data = UInt(OUTPUT, width=params.dataBits)
+  // 1位的last信号
   val last = Bool(OUTPUT)
+  // x = trans
+  // 已经传输的节拍数量
   val beats = UInt(OUTPUT, width=params.xferBits + 1)
 }
 
 class CreditBump(params: ChipLinkParams) extends GenericParameterizedBundle(params) {
+  // 加上Credit后封装？
+  // CreditBits目前是20，意思说有20M的Buffer项可以用
   val a = UInt(OUTPUT, width = params.creditBits)
   val b = UInt(OUTPUT, width = params.creditBits)
   val c = UInt(OUTPUT, width = params.creditBits)
@@ -32,6 +42,7 @@ class CreditBump(params: ChipLinkParams) extends GenericParameterizedBundle(para
   def X: Seq[UInt] = Seq(a, b, c, d, e)
 
   // saturating addition
+  // 饱和计数器
   def +(that: CreditBump): CreditBump = {
     val out = Wire(new CreditBump(params))
     (out.X zip (X zip that.X)) foreach { case (o, (x, y)) =>
@@ -44,12 +55,18 @@ class CreditBump(params: ChipLinkParams) extends GenericParameterizedBundle(para
   // Send the MSB of the credits
   def toHeader: (UInt, CreditBump) = {
     def msb(x: UInt) = {
+      // rightOR: 找到最左边的1，然后把右边全部填为1
       val mask = rightOR(x) >> 1
+      // maskOH: 把最高位的1找出来，其他的清除，是一个one-hot编码
       val msbOH = ~(~x | mask)
       val msb = OHToUInt(msbOH << 1, params.creditBits + 1) // 0 = 0, 1 = 1, 2 = 4, 3 = 8, ...
+      // 把位宽拓宽到5位
       val pad = (msb | UInt(0, width=5))(4,0)
+      // 返回pad后的5位msb标志，和清除了msb的x
       (pad, x & mask)
     }
+    // 猜测一次清除掉x的一个msb，然后返回剩下的部分(x_rest)
+    // 例如，01011,toHeader之后，就会返回3和00011
     val (a_msb, a_rest) = msb(a)
     val (b_msb, b_rest) = msb(b)
     val (c_msb, c_rest) = msb(c)
@@ -72,6 +89,7 @@ class CreditBump(params: ChipLinkParams) extends GenericParameterizedBundle(para
 
 object CreditBump {
   def apply(params: ChipLinkParams, x: Int): CreditBump = {
+    // 指定Credit的初始值，初始化每个通道对应的Credit，然后返回一个Credit Bump
     val v = UInt(x, width = params.creditBits)
     val out = Wire(new CreditBump(params))
     out.X.foreach { _ := v }
@@ -79,6 +97,8 @@ object CreditBump {
   }
 
   def apply(params: ChipLinkParams, header: UInt): CreditBump = {
+    // 把header的credit转换过来
+    // 和toHeader互为反函数
     def convert(x: UInt) =
       Mux(x > UInt(params.creditBits),
           ~UInt(0, width = params.creditBits),
--- src/main/scala/chiplink/TX.scala
+++ src/main/scala/chiplink/TX.scala
@@ -25,7 +25,7 @@ class TX(info: ChipLinkInfo) extends Module
     val sc = DecoupledIO(new DataLayer(info.params)).flip
     val sd = DecoupledIO(new DataLayer(info.params)).flip
     val se = DecoupledIO(new DataLayer(info.params)).flip
-    // TODO: 什么是Credit
+    // TODO: 什么是Credit (Done)
     val rxc = new AsyncBundle(new CreditBump(info.params), AsyncQueueParams.singleton()).flip
     val txc = new AsyncBundle(new CreditBump(info.params), AsyncQueueParams.singleton()).flip
   }
@@ -37,6 +37,7 @@ class TX(info: ChipLinkInfo) extends Module
 
   // Constantly pull credits from RX
   // 从Rx轮训Credits
+  // rxInCredits
   val rxInc = FromAsyncBundle(io.rxc)
   val txInc = FromAsyncBundle(io.txc)
   rxInc.ready := Bool(true)
@@ -45,6 +46,7 @@ class TX(info: ChipLinkInfo) extends Module
   // Cross the requests (if necessary)
   val sync = info.params.syncTX
   // Use different Queue according to whether it is sync
+  // 输入的通道a,b,c,d,e
   val qa = if (sync) ShiftQueue(io.sa, 2) else FromAsyncBundle(io.a)
   val qb = if (sync) ShiftQueue(io.sb, 2) else FromAsyncBundle(io.b)
   val qc = if (sync) ShiftQueue(io.sc, 2) else FromAsyncBundle(io.c)
@@ -53,10 +55,19 @@ class TX(info: ChipLinkInfo) extends Module
   private def qX = Seq(qa, qb, qc, qd, qe)
 
   // Consume TX credits and propagate pre-paid requests
+  // 先把TX这边的Credit给使用了，然后把request传下去
+  // qX: 所有队列 
+  // 每一个通道都有20个Credits bits
   val ioX = (qX zip (tx.X zip txInc.bits.X)) map { case (q, (credit, gain)) =>
+    // Example: (tilelink bundle, (credits bits for all channel, txInCredits for all channel))
+    // 也就是tilelink通道，模块维护的每个通道的credits和得到的credits
+    // 全部传输完成之后拉高first
     val first = RegEnable(q.bits.last, Bool(true), q.fire())
+    // 剩下的credit数量
     val delta = credit -& q.bits.beats
+    // 是否允许进入队列
     val allow = !first || (delta.asSInt >= SInt(0))
+    // 更新该通道剩余的Credit（饱和计数）
     credit := Mux(q.fire() && first, delta, credit) + Mux(txInc.fire(), gain, UInt(0))
 
     val cq = Module(new ShiftQueue(q.bits.cloneType, 2)) // maybe flow?
@@ -67,12 +78,18 @@ class TX(info: ChipLinkInfo) extends Module
   }
 
   // Prepare RX credit update headers
+  // 更新对面端的RX Credit的消息
+  // 如何去封装header呢?
+  // rxQ是最终发送出去的东西
+  // “贪心”地每一次尽可能地归还最大的credit（2的n次方）
   val rxQ = Module(new ShiftQueue(new DataLayer(info.params), 2)) // maybe flow?
   val (rxHeader, rxLeft) = rx.toHeader
+  // 不停地往rxq里塞东西，塞更新后的header
   rxQ.io.enq.valid := Bool(true)
   rxQ.io.enq.bits.data  := rxHeader
   rxQ.io.enq.bits.last  := Bool(true)
   rxQ.io.enq.bits.beats := UInt(1)
+  // 更新Credits
   rx := Mux(rxQ.io.enq.fire(), rxLeft, rx) + Mux(rxInc.fire(), rxInc.bits, CreditBump(info.params, 0))
 
   // Include the F credit channel in arbitration
@@ -95,12 +112,20 @@ class TX(info: ChipLinkInfo) extends Module
   rxQ.io.deq.ready := f.ready && allowReturn
 
   // Select a channel to transmit from those with data and space
+  // 仲裁：从6个通道里选一个发送出去
   val first = RegInit(Bool(true))
+  // state: 目前正在发送的那个通道(ABCDEF之一，所以是6位)
   val state = Reg(UInt(0, width=6))
+  // 从6个request里选一个，first是上次被选中的那个会参与？
   val readys = TLArbiter.roundRobin(6, requests, first)
+  // winnder：被选中的通道，用one hot编码
   val winner = readys & requests
+  // grant: 被选中的，因为winner是组合逻辑，state是时序逻辑，这个只是起到了一个hold的作用而已
+  // grant = hold(winner)
   val grant = Mux(first, winner, state)
+  // allowed = hold(state)
   val allowed = Mux(first, readys, state)
+  // 被仲裁选中允许传输的队列
   (ioF zip allowed.asBools) foreach { case (beat, sel) => beat.ready := sel }
 
   val send = Mux(first, rxQ.io.deq.valid, (state & requests) =/= UInt(0))
